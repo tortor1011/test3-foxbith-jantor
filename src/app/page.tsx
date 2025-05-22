c47e5ba0075cd8476f82f7160c7626c52efe0c30
tortor1011 <raung1411@gmail.com>
2025-05-22 18:27:47 +0700

    roll back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import QuestionnaireForm from "./components/questionnaire";
 import FormQuestion from "./components/form"
+import Only from "./components/onlyform"
 
 
 export default function Home() {
   return (
     <div >
-      <FormQuestion />
-      
+      <Only />
+
     </div>
   );
 }
